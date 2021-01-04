1e60cb793ce14843ed231005d12c69490c619574
Nv7 <junk4Nv7@gmail.com>
2021-01-04 11:34:37 -0800

    Goplus demo

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import r "github.com/lachee/raylib-goplus/raylib"
 
+func main() {
+	r.InitWindow(800, 450, "Raylib Go Plus")
+	for !r.WindowShouldClose() {
+		r.BeginDrawing()
+		r.ClearBackground(r.RayWhite)
+		r.DrawText("Woo! Raylib-Go-Plus! Now with ++", 20, 20, 20, r.GopherBlue)
+		r.EndDrawing()
+	}
+	r.CloseWindow()
 }
